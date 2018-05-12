aba0074d9c7d6fa14154818edae35db8a68f7411
prief <207792089@qq.com>
2018-05-12 11:52:09 +0800

    修复支付和帮扶对象信息

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 55556,
+    port: 55557,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
